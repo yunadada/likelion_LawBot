bbb82447db990773993aa6c291f240a707a532ee
yunadada <ynson0216@naver.com>
2025-07-18 15:41:24 +0900

    feat: 메인페이지 UI 구현

--- src/components/DView/DView.jsx
+++ src/components/DView/DView.jsx
@@ -1,5 +1,62 @@
+import "./DView.css";
+import { BsDownload } from "react-icons/bs";
+
 const DView = () => {
-  return <div>DView</div>;
+  return (
+    <div className="background">
+      <div className="container">
+        <div className="title">
+          <h2>📘 내 문서 목록 조회</h2>
+          <button> 문서 업로드</button>
+        </div>
+        <div className="content">
+          <table className="table">
+            <colgroup>
+              <col style={{ width: "65%" }} />
+              <col style={{ width: "25%" }} />
+              <col style={{ width: "10%" }} />
+            </colgroup>
+            <thead>
+              <tr>
+                <th>파일 이름</th>
+                <th>올린 날짜</th>
+                <th>다운로드</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>근로계약서_최종본.docx</td>
+                <td>30/12/2024</td>
+                <td>
+                  <button className="downloadButton">
+                    <BsDownload />
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>근로계약서_최종본.docx</td>
+                <td>30/12/2024</td>
+                <td>
+                  <button className="downloadButton">
+                    <BsDownload />
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>근로계약서_최종본.docx</td>
+                <td>30/12/2024</td>
+                <td>
+                  <button className="downloadButton">
+                    <BsDownload />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DView;
