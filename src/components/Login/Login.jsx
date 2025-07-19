7664541c7c0ee5b88abad31eecc537d4b87f663b
JiYi Kim <20230299@kumoh.ac.kr>
2025-07-19 20:55:50 +0900

    feat: 로그인/회원가입 UI 및 기능 구현 완료

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,43 @@
+import "./Login.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://서버주소/api/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("accessToken", res.data.accessToken);
+      navigate("/");
+    } catch (err) {
+      alert("로그인에 실패하였습니다. 다시 확인해주세요.");
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <h1>로그인</h1>
+    <div className="auth-container">
+      <div className="auth-box">
+        <h2>로그인</h2>
+        <input
+          type="email"
+          placeholder="이메일 입력"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호 입력"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>로그인</button>
       </div>
     </div>
   );
