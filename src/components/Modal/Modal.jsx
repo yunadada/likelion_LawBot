d87d0026fde0d76f1d93770457369a348acd2805
yunadada <ynson0216@naver.com>
2025-07-21 02:30:11 +0900

    fix: fetch를 axios로 변경 및 status 비교 값 수정

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,6 @@ import { useRef, useState, useEffect } from "react";
 import "./Modal.css";
 import { IoCloseOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
-import Loading from "../Loading/Loading";
 import { FaDownload } from "react-icons/fa6";
 
 const Modal = ({ onClose }) => {
@@ -10,7 +9,6 @@ const Modal = ({ onClose }) => {
   const fileInputRef = useRef(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [fileId, setFileId] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const clickBackground = (e) => {
@@ -36,22 +34,28 @@ const Modal = ({ onClose }) => {
     formData.append("file", selectedFile);
 
     try {
-      const res = await fetch("http://localhost:8080/api/documents/upload", {
-        method: "POST",
-        body: formData,
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      });
+      const accessToken = localStorage.getItem("accessToken");
 
-      const data = await res.json();
-      setFileId(data.fileId);
-      setIsLoading(true);
+      const res = await axios.post(
+        "http://localhost:8080/api/documents/upload",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
-      if (data.status === "completed") {
-        navigate(`/result/${data.fileId}`);
-      } else {
-        setIsLoading(true);
+      const data = res.data;
+      setFileId(data.id);
+
+      if (data.status === "UPLOADED") {
+        navigate("/loading", {
+          state: {
+            fileId: data.id,
+          },
+        });
       }
     } catch (err) {
       console.error("업로드 실패", err);
@@ -59,77 +63,43 @@ const Modal = ({ onClose }) => {
     }
   };
 
-  // 상태 폴링
-  useEffect(() => {
-    if (!fileId || !isLoading) return;
-
-    const interval = setInterval(async () => {
-      try {
-        const res = await fetch(
-          `http://localhost:8080/api/documents/status/${fileId}`,
-          {
-            headers: {
-              Authorization: localStorage.getItem("accessToken"),
-            },
-          }
-        );
-
-        const data = await res.json();
-        if (data.status === "completed") {
-          clearInterval(interval);
-          navigate(`/result/${fileId}`);
-        }
-      } catch (e) {
-        console.error("상태 확인 실패", e);
-      }
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [fileId, isLoading]);
-
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div
-          className="modalBackground"
-          ref={modalBackground}
-          onClick={clickBackground}
-        >
-          <div className="modal">
-            <div className="modalTitle">
-              <p>문서 업로드</p>
-              <button onClick={onClose}>
-                <IoCloseOutline />
-              </button>
-            </div>
+    <div
+      className="modalBackground"
+      ref={modalBackground}
+      onClick={clickBackground}
+    >
+      <div className="modal">
+        <div className="modalTitle">
+          <p>문서 업로드</p>
+          <button onClick={onClose}>
+            <IoCloseOutline />
+          </button>
+        </div>
 
-            <div className="contents">
-              <div className="selectFile">
-                <label>
-                  파일 선택
-                  <input type="file" ref={fileInputRef} onChange={uploadFile} />
-                </label>
-              </div>
+        <div className="contents">
+          <div className="selectFile">
+            <label>
+              파일 선택
+              <input type="file" ref={fileInputRef} onChange={uploadFile} />
+            </label>
+          </div>
 
-              <div className="docs">
-                {selectedFile ? selectedFile.name : "선택된 파일이 없습니다."}
-              </div>
+          <div className="docs">
+            {selectedFile ? selectedFile.name : "선택된 파일이 없습니다."}
+          </div>
 
-              <div className="uploadButton">
-                <button onClick={uploadSelectedFile}>
-                  <div className="buttonInner">
-                    <FaDownload />
-                    <p>업로드</p>
-                  </div>
-                </button>
+          <div className="uploadButton">
+            <button onClick={uploadSelectedFile}>
+              <div className="buttonInner">
+                <FaDownload />
+                <p>업로드</p>
               </div>
-            </div>
+            </button>
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
