e442ab30c105b9351166efd5d15471614651902d
yunadada <ynson0216@naver.com>
2025-07-20 18:50:57 +0900

    chore: 임시 테스트용 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import DUpload from "./components/DUpload/DUpload";
 import DView from "./components/DView/DView";
-import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Loading from "./components/Loading/Loading";
+import ResultLayout from "./components/ResultLayout/ResultLayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DView />} />
+      <Route path="/result" element={<ResultLayout />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dview" element={<DView />} />
-      <Route path="/dupload" element={<DUpload />} />
       <Route path="/loading" element={<Loading />} />
     </Routes>
   );
